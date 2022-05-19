82a01162b6722b522c281002a8027b3d718c461a
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-19 09:33:29 -0300

    Adiciona requisito 4.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -26,6 +27,15 @@ class Header extends React.Component {
        <header data-testid="header-component">
          { loading ? <Loading />
            : <p data-testid="header-user-name">{ user }</p> }
+         <nav>
+           <ul>
+             <li><Link to="/search" data-testid="link-to-search">Search</Link></li>
+             <li>
+               <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+             </li>
+             <li><Link to="/profile" data-testid="link-to-profile">Profile</Link></li>
+           </ul>
+         </nav>
        </header>
 
      );
