d034ca632f9706327464735d50ddeea3224cffd3
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-19 20:17:47 -0300

    Adiciona requisito 6.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Loading extends React.Component {
   render() {
     return (
-      <p>Carregando...</p>
+      <h1>Carregando...</h1>
     );
   }
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends React.Component {
   state = {
     inputArtist: '',
     isDisable: true,
+    loading: false,
+    albumList: [],
+    artist: '',
+    // invalidArtist: '',
   }
 
   handleChange = ({ target }) => {
@@ -18,29 +25,66 @@ class Search extends React.Component {
     });
   };
 
+  handleClick = async (inputArtist) => {
+    this.setState({ loading: true });
+    const albuns = await searchAlbumsAPI(inputArtist);
+    this.setState({
+      albumList: [...albuns],
+      loading: false,
+      inputArtist: '',
+    });
+    if (inputArtist.length > 0) {
+      this.setState({ artist: inputArtist });
+    }
+  }
+
   render() {
-    const { isDisable, inputArtist } = this.state;
+    const { isDisable, inputArtist, loading, albumList, artist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <form>
-          <label htmlFor="search-artist-input">
+          <label htmlFor="search-artist-Input">
             <input
               type="text"
               data-testid="search-artist-input"
-              name={ inputArtist }
+              value={ inputArtist }
               onChange={ this.handleChange }
             />
+
           </label>
           <button
-            type="submit"
+            type="button"
             data-testid="search-artist-button"
             value="Pesquisar"
             disabled={ isDisable }
+            onClick={ async () => { await this.handleClick(inputArtist); } }
+            // onClick={ this.handleClick }
           >
             Pesquisar
           </button>
+          { loading && <Loading /> }
         </form>
+        { albumList.length > 0 ? (
+          <h3>
+            Resultado de álbuns de:
+            {' '}
+            { artist }
+          </h3>) : <p>Nenhum álbum foi encontrado</p> }
+        { albumList
+          .map(({ artworkUrl100, collectionName, artistName, collectionId }, index) => (
+            (
+              <Link
+                key={ index }
+                to={ `/album/${collectionId}` }
+                data-testid={ `link-to-album-${collectionId}` }
+              >
+                <img src={ artworkUrl100 } alt={ collectionName } />
+                <p>{ collectionName }</p>
+                <p>{ artistName }</p>
+              </Link>
+            )
+          ))}
       </div>
     );
   }
