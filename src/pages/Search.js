69d1e3efd1d4b87cf8c71a4c2fcb06469834c902
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-19 09:59:13 -0300

    Adiciona requisito 5.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,45 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    inputArtist: '',
+    isDisable: true,
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ inputArtist: value }, () => {
+      if (value.length >= 2) {
+        this.setState({ isDisable: false });
+      } else {
+        this.setState({ isDisable: true });
+      }
+    });
+  };
+
   render() {
+    const { isDisable, inputArtist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <form>
+          <label htmlFor="search-artist-input">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              name={ inputArtist }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            value="Pesquisar"
+            disabled={ isDisable }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
